ccec2c4274f339b4f3e14d5538969d70fe773647
Vidhan <vidhanlonare@gmail.com>
2026-03-28 18:18:46 +0530

    add UserResponse and SecurityConfig to bypass authentication

--- src/main/java/com/instagram/clone/controller/UserController.java
+++ src/main/java/com/instagram/clone/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.instagram.clone.controller;
 
 import com.instagram.clone.dto.UserRegisterRequest;
+import com.instagram.clone.dto.UserResponse;
 import com.instagram.clone.entity.User;
 import com.instagram.clone.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -14,7 +15,15 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/register")
-    public User register(@RequestBody UserRegisterRequest request) {
-        return userService.register(request);
+    public UserResponse register(@RequestBody UserRegisterRequest request) {
+        User user = userService.register(request);
+        return UserResponse.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .bio(user.getBio())
+                .isPrivate(user.isPrivate())
+                .createdAt(user.getCreatedAt())
+                .build();
     }
 }
---
+++ src/main/java/com/instagram/clone/dto/UserResponse.java
@@ -0,0 +1,18 @@
+package com.instagram.clone.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class UserResponse {
+
+    private Long id;
+    private String username;
+    private String email;
+    private String bio;
+    private boolean isPrivate;
+    private LocalDateTime createdAt;
+}
