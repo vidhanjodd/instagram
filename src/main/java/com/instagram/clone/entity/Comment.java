f2d86f9cc46782039576c8af81294cb53113f03a
Vidhan <vidhanlonare@gmail.com>
2026-03-31 10:42:37 +0530

    add comment entity and changed post entity user id to user object

---
+++ src/main/java/com/instagram/clone/entity/Comment.java
@@ -0,0 +1,45 @@
+package com.instagram.clone.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "comments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Comment parent;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+    private List<Comment> replies;
+
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
--- src/main/java/com/instagram/clone/entity/Post.java
+++ src/main/java/com/instagram/clone/entity/Post.java
@@ -26,7 +26,9 @@ public class Post {
 
     private String publicId; // from Cloudinary
 
-    private Long userId;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
     private LocalDateTime createdAt;
 }
