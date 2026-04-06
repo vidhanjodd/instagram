1b949fa9adfc48212ef8674de2d781cf5ca89c7a
ayush.18 <ayush.dogra.36.2@mountblue.tech>
2026-04-06 16:06:58 +0530

    update websocketconfig

--- src/main/java/com/instagram/clone/dto/ChatMessage.java
+++ src/main/java/com/instagram/clone/dto/ChatMessage.java
@@ -1,32 +1,3 @@
-//package com.instagram.clone.dto;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Builder;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//
-//import java.time.LocalDateTime;
-//
-//@Getter
-//@Setter
-//@Builder
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class ChatMessage {
-//    private Long id;
-//    private Long senderId;
-//    private String senderUsername;
-//    private String senderProfilePic;
-//    private Long receiverId;
-//    private String receiverUsername;
-//    private String content;
-//    private LocalDateTime createdAt;
-//}
-
-
-
-
 
 package com.instagram.clone.dto;
 
--- src/main/java/com/instagram/clone/entity/Message.java
+++ src/main/java/com/instagram/clone/entity/Message.java
@@ -13,7 +13,6 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 public class Message {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
